770e31f70769bb6511d8f806a7e4de2a739f537b
Ramadan <daudalaramadan2@gmail.com>
2024-01-28 21:45:21 +0000

    placed the items in the center and added some styles to it

---
+++ src/Components/Signup-Login/LoginSignup.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import './LoginSignup.css'/////this imports the css components from LoginSignup.css into this jsx
+import email_icon from '../Assets/email.png'
+import password_icon from '../Assets/password.png'
+import person_icon from '../Assets/person.png'
+export const LoginSignup = () => {
+  return (
+    ////crreating the html code for our component
+    <div className='container'>
+        <div className="header">
+            <div className="text">Sign-Up</div>
+            <div className="underline"></div>
+        </div>
+        <div className="inputs">
+            <div className="input">
+                <img src={person_icon} alt="" />
+                <input type="text" />
+            </div>
+            <div className="input">
+                <img src={email_icon} alt="" />
+                <input type="email" />
+            </div>
+            <div className="input">
+                <img src={password_icon} alt="" />
+                <input type="password" />
+            </div>
+        </div>
+        <div className="forgot-password">lost Passwrod? <span>Click here</span></div>
+        <div className="submit-container">
+            <div className="submit">Sign Up</div>
+            <div className="submit">Login</div>
+        </div>
+
+    </div>
+  )
+}
+
+
